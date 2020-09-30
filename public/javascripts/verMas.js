b23a70110927e551af45103e2f89bab8a9acc458
Fedecolombo1 <colombofederico17@gmail.com>
2020-09-30 18:22:01 -0300

    primera version

--- public/javascripts/verMas.js
+++ public/javascripts/verMas.js
@@ -1,18 +1,34 @@
 window.addEventListener("load", function(){
-    var verMas = document.querySelector("button.col-5.col-lg-2.boton.srvbtn.verMas")
+    var verTodas = document.querySelector("button.col-5.col-lg-2.boton.srvbtn.verTodas")
     var verMenos = document.querySelector("button.col-5.col-lg-2.boton.srvbtn.verMenos")
-    var div = document.querySelector("div.col-12.cerrado")
+    var div = document.querySelector("div.row.col-12.align.collapse")
 
-    verMas.addEventListener("click", function(){
-        div.classList.remove("cerrado")
-        div.classList.add("abierto");
-        verMas.style.display = "none"
+    /*verTodas.addEventListener("click", function(){
+        verTodas.style.display = "none"
+        verMenos.style.display = "inline-block"
     })
     
     verMenos.addEventListener("click", function(){
-        div.classList.remove("abierto")
-        div.classList.add("cerrado");
-        verMas.style.display = "inline"
-    })
+        
+        verTodas.style.display = "inline"
+        verMenos.style.display = "none"
+    })*/
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+    }
+
+
 })
 
